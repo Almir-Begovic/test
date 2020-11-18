08c6fe5298f271821120845af4190ea71ee1f143
almir begovic <begovic92@gmail.com>
2020-11-18 05:57:58 +0000

    Recreated app

--- SearchApi/Controllers/ValuesController.cs
+++ SearchApi/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Search_Api.Controllers
+namespace SearchApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
--- SearchApi/Startup.cs
+++ SearchApi/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace Search_Api
+namespace SearchApi
 {
     public class Startup
     {
